6c475eb0c10f76c113369bfc9ac967ade3bf292e
feh-franc0 <fernandofv1110@gmail.com>
2023-03-06 17:15:27 -0300

    Realizando queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import fastify from 'fastify'
+// import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
 
-  return tables
+  // const transaction = await knex('transactions').select('*')
+
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+
+  return transaction
 })
 
 app
